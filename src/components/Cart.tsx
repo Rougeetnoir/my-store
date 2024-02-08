c4cb3d26590838bbad4472531facd218b320a28c
Rougeetnoir <yangyaner1@gmail.com>
2024-02-08 13:44:39 -0800

    Adding image and link when cart it empty

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -6,10 +6,11 @@ import { Separator } from "./ui/separator"
 import { formatPrice } from "@/lib/utils"
 import Link from "next/link"
 import { buttonVariants } from "./ui/button"
+import Image from "next/image"
 
 const Cart = () => {
 
-    const itemCount = 1
+    const itemCount = 0
     const fee = 1
     return (
         <Sheet>
@@ -61,7 +62,32 @@ const Cart = () => {
                         </div>
                     </>
                 ) : (
-                    <div></div>
+                    <div className="flex h-full flex-col items-center justify-center space-y-1">
+                        <div
+                            aria-hidden="true"
+                            className="relative mb-4 h-60 w-60 tetx-muted-foreground">
+                            <Image
+                                src="/hippo-empty-cart.png"
+                                fill
+                                alt="hippo empty cart"
+                            />
+                        </div>
+                        <div className="text-xl font-semibold">
+                            Your cart is empty
+                        </div>
+                        <SheetTrigger asChild>
+                            <Link
+                                href='/products'
+                                className={buttonVariants({
+                                    variant: "link",
+                                    size: "sm",
+                                    className:
+                                        "text-sm text-muted-foreground",
+                                })}>
+                                Add items to your cart to checkout
+                            </Link>
+                        </SheetTrigger>
+                    </div>
                 )}
             </SheetContent>
         </Sheet>
